347d3d3ecf32a30ba1eed4f2acfb95ca48b99072
mustokrakish <sergeypolish@gmail.com>
2018-03-29 03:28:14 +0300

    upd lab01

--- lab01.py
+++ lab01.py
@@ -1,15 +1,15 @@
 import datetime
 a = input('Enter the year of your birth here:')
-
 try:
     a = int(a)
+    a = datetime.datetime.today().year - a
+    if a < 18:
+        print('Your are Underage. Your age is', a, '...too low to access')
+    elif a > 100:
+        print('You are dinosaur. Your age', a, '?!')
+    else:
+        print('You are adult. Your age:', a, ':)')
 except ValueError:
     print('Error. Try Again')
-a = datetime.datetime.today().year - a
-if a < 18:
-    print('Your are Underage. Your age is', a, '...too low to access')
-elif a > 100:
-    print('You are dinosaur. Your age', a, '?!')
-else:
-    print('You are adult. Your age:', a, ':)')
+
 
