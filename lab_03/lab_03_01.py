827194692b4bbc20704df6ef40515660c53d2632
mustokrakish <sergeypolish@gmail.com>
2018-04-11 21:57:15 -0400

    is_prime

---
+++ lab_03/lab_03_01.py
@@ -0,0 +1,20 @@
+# Написать функцию is_prime, принимающую 1 аргумент — число от 0 до 1000, и возвращающую True,
+# если оно простое, и False - иначе.
+
+
+def is_prime(a):
+    if a == 0 or a == 1:
+        return False
+    list_1 = [i for i in range(2, a)]
+    print(list_1)
+    for i in list_1:
+        if a % i == 0:
+            return False
+        else:
+            return True
+
+
+if __name__ == "__main__":
+    print(is_prime(28))
+
+
